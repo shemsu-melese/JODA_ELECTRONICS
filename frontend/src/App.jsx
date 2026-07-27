ea46d21318e22a19d7bb2aae2c70d07ef6b8c2e4
Shemsu Melese <shemsumelese6@gmail.com>
2026-07-28 02:39:44 +1100

    intial project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+       <h1 className='heading'>WELCOME TO <span id='joda'>JODA ELECTRONCS</span></h1>
+    </div>
+  )
+}
+
+export default App
+
